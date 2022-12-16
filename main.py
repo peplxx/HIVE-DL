9fc818e2ef5056425664aa1151d0b497190ccd6e
KetrinXD <seregaopky@mail.ru>
2022-12-16 14:24:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import multiprocessing as mp
 
 
 def invert(xyz, a):
-    return [xyz[0] + a, xyz[1] + a, xyz[2] + a] + xyz[3:]
+    return [xyz[0] + a, xyz[1], xyz[2]] + xyz[3:]
 
 
 async def main():
@@ -19,20 +19,19 @@ async def main():
         await sim.setInt32Param(sim.intparam_idle_fps, 0)
 
         print("Success hooked client ...")
-
-        drones = [Drone("copter", sim), Drone("copter2", sim), Drone("copter3", sim),
-                  Drone("copter4", sim), Drone("copter5", sim), Drone("copter6", sim)]
+        num_of_drones = 6
+        drones = [Drone(f"copter{i}", sim) for i in range(1, num_of_drones + 1)]
         await asyncio.gather(*[
             drone.set_target_and_object() for drone in drones
         ])
         print("Initialized all drones...")
-        path = [(1, 1, 1, 0, 0, 0), (2, 2, 2, 0, 0, 0), (1, 1, 1, 0, 0, 0), (0, 0, 0, 0, 0, 0)]
+        path = [(1, 1, 1, 0, 0, 0), (1, 10, 2, 0, 0, 0), (1, 1, 1, 0, 0, 0)]
         for xyz in path:
             new_pos = list(xyz)
             new_pos.append(-1)
             await asyncio.gather(*[
                 drone.move_target(invert(new_pos, i))
-                for i, drone in enumerate(drones,1)
+                for i, drone in enumerate(drones, 1)
             ])
     print("Program ended")
 
